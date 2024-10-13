68bd4d877df2250d7dec149ac94c3bde6ea75b46
Karacs Péter <karacspeter@MacBook-Pro-46.local>
2024-10-13 19:20:35 +0200

    comm

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style>
-/* Importt Bootstrapw CSS */
+/* Import Bootstrap CSS */
 @import url("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css");
 
 /*stylesheet*/
